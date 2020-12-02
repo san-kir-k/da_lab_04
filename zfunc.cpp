1ee5b6ebc014d94ea6bbda10e7f9270cbd8b4b9c
san-kir-k <dfomka@list.ru>
2020-12-02 18:18:48 +0300

    Add namespace

--- zfunc.cpp
+++ zfunc.cpp
@@ -1,39 +1,41 @@
 #include "zfunc.hpp"
 
-void ZFunction(const std::vector<TUll>& pattern, std::vector<TUll>& zVec) {
-    for (TUll i = 0; i < pattern.size(); ++i) {
-        zVec.push_back(0);
-    }
-    TUll left = 0;
-    TUll right = 0;
-    for (TUll k = 1; k < pattern.size(); ++k) {
-        if (k > right) {
-            TUll j = 0;
-            TUll i = k;
-            while (pattern[i] == pattern[j]) {
-                i++;
-                j++;
-            }
-            zVec[k] = j;
-            if (zVec[k] > 0) {
-                right = k + zVec[k] - 1;
-                left = k;
-            }
-        } else {
-            TUll matchingSuffixLen = right - k + 1;
-            TUll kPrefix = k - left;
-            if (zVec[kPrefix] < matchingSuffixLen) {
-                zVec[k] = zVec[kPrefix];
-            } else {
-                TUll j = matchingSuffixLen;
-                TUll i = right + 1;
+namespace ZF {
+    void ZFunction(const std::vector<TUll>& pattern, std::vector<TUll>& zVec) {
+        for (TUll i = 0; i < pattern.size(); ++i) {
+            zVec.push_back(0);
+        }
+        TUll left = 0;
+        TUll right = 0;
+        for (TUll k = 1; k < pattern.size(); ++k) {
+            if (k > right) {
+                TUll j = 0;
+                TUll i = k;
                 while (pattern[i] == pattern[j]) {
                     i++;
                     j++;
                 }
-                zVec[k] = i - k;
-                right = i - 1;
-                left = k;
+                zVec[k] = j;
+                if (zVec[k] > 0) {
+                    right = k + zVec[k] - 1;
+                    left = k;
+                }
+            } else {
+                TUll matchingSuffixLen = right - k + 1;
+                TUll kPrefix = k - left;
+                if (zVec[kPrefix] < matchingSuffixLen) {
+                    zVec[k] = zVec[kPrefix];
+                } else {
+                    TUll j = matchingSuffixLen;
+                    TUll i = right + 1;
+                    while (pattern[i] == pattern[j]) {
+                        i++;
+                        j++;
+                    }
+                    zVec[k] = i - k;
+                    right = i - 1;
+                    left = k;
+                }
             }
         }
     }
--- zfunc.hpp
+++ zfunc.hpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <algorithm>
 
-using TUll = unsigned long long;
-
-void ZFunction(const std::vector<TUll>& pattern, std::vector<TUll>& zVec);
+namespace ZF {
+    using TUll = unsigned long long;
+    
+    void ZFunction(const std::vector<TUll>& pattern, std::vector<TUll>& zVec);
+}
